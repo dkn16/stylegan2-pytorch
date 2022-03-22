540c9002acbf92392b8dbec8b4c3fe80d9c6ad92
dkn <dkn16@foxmail.com>
2022-03-22 23:39:25 +0800

    modification of the lmdb dataset

--- prepare_21cmdata.py
+++ prepare_21cmdata.py
@@ -2,6 +2,8 @@ import argparse
 from io import BytesIO
 import multiprocessing
 from functools import partial
+import os
+import numpy as np
 
 from PIL import Image
 import lmdb
@@ -40,27 +42,34 @@ def resize_worker(img_file, sizes, resample):
     return i, out
 
 
-def prepare(
-    env, dataset, n_worker, sizes=(128, 256, 512, 1024), resample=Image.LANCZOS
+def prepare21cm(
+    env, datapath, size=[64]
 ):
-    resize_fn = partial(resize_worker, sizes=sizes, resample=resample)
+    # get the filenames in the datapath
+    filenames=[]
+    for root, dirs, files in os.walk(datapath, topdown=False):
+        for name in files:
+            filenames.append(os.path.join(root, name))
 
-    files = sorted(dataset.imgs, key=lambda x: x[0])
-    files = [(i, file) for i, (file, label) in enumerate(files)]
-    total = 0
+    #check the dimension of data
+    datacache=np.load(filenames[0])
+    if datacache.ndim!=3:
+        print('Please check the dimension of 21cm cube, should be 3D')
+        exit
+    slices=np.shape(datacache)[0]
 
-    with multiprocessing.Pool(n_worker) as pool:
-        for i, imgs in tqdm(pool.imap_unordered(resize_fn, files)):
-            for size, img in zip(sizes, imgs):
-                key = f"{size}-{str(i).zfill(5)}".encode("utf-8")
-
-                with env.begin(write=True) as txn:
-                    txn.put(key, img)
-
+    #load the 21cm datacubes
+    total=0
+    for filename in filenames:
+        datacache=np.load(filename)
+        for i in range(slices):
+            key = f"{size[0]}-{str(total).zfill(6)}".encode("utf-8")
+            with env.begin(write=True) as txn:
+                txn.put(key, datacache[i])
             total += 1
 
-        with env.begin(write=True) as txn:
-            txn.put("length".encode("utf-8"), str(total).encode("utf-8"))
+    with env.begin(write=True) as txn:
+        txn.put("length".encode("utf-8"), str(total).encode("utf-8"))
 
 
 if __name__ == "__main__":
@@ -69,33 +78,23 @@ if __name__ == "__main__":
     parser.add_argument(
         "--size",
         type=str,
-        default="128,256,512,1024",
+        default="64",
         help="resolutions of images for the dataset",
     )
-    parser.add_argument(
-        "--n_worker",
-        type=int,
-        default=8,
-        help="number of workers for preparing dataset",
-    )
-    parser.add_argument(
-        "--resample",
-        type=str,
-        default="lanczos",
-        help="resampling methods for resizing images",
-    )
+    #deleted the multiprocessing argument, because i'm too stupid to use multiprocessing
+    #deleted the resampling argument, not needed anymore
     parser.add_argument("path", type=str, help="path to the image dataset")
 
     args = parser.parse_args()
 
-    resample_map = {"lanczos": Image.LANCZOS, "bilinear": Image.BILINEAR}
-    resample = resample_map[args.resample]
+    #resample_map = {"lanczos": Image.LANCZOS, "bilinear": Image.BILINEAR}
+    #resample = resample_map[args.resample]
 
     sizes = [int(s.strip()) for s in args.size.split(",")]
 
     print(f"Make dataset of image sizes:", ", ".join(str(s) for s in sizes))
 
-    imgset = datasets.ImageFolder(args.path)
+    imgset = args.path
 
     with lmdb.open(args.out, map_size=1024 ** 4, readahead=False) as env:
-        prepare(env, imgset, args.n_worker, sizes=sizes, resample=resample)
+        prepare21cm(env, imgset, size=sizes)
